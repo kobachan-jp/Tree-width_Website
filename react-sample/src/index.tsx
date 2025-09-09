fe7a968b615cb665a9e9eb91377f100e87768f1f
kobachan <shoko.kobachan@gmail.com>
2025-09-09 17:11:45 +0900

    section3.2 finished

--- react-sample/src/index.tsx
+++ react-sample/src/index.tsx
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './components/Hello';
+import Name from './components/Name';
+import Message from './components/Message';
+import Parent from './components/ContainerSample';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/*AppからHelloに置き換える*/}
+    <Hello />
+    <Name />
+    <Message/>
+    <Parent/>
   </React.StrictMode>
 );
 
