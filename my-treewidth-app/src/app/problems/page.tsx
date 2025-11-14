0df028facba40a79b41bbf8db0e0d8bda7caf25e
kobachan <shoko.kobachan@gmail.com>
2025-11-14 18:49:23 +0900

    prettierの追加

--- my-treewidth-app/src/app/problems/page.tsx
+++ my-treewidth-app/src/app/problems/page.tsx
@@ -9,23 +9,32 @@ type Problem = {
 };
 
 export default function ProblemsPage(){
+  //問題の配列を状態管理
   const [problems, setProblems] = useState<Problem[]>([]);
-  const [message,setMessage] = useState("");
+  //正答判定をProblem.idをキーとするmapでbooleanまたはundefinedを管理
+  const [messages,setMessages] = useState<{[id:number]:boolean | undefined}>([]);
 
+  //GETリクエストで問題一覧を取得.
   useEffect(() => {
     fetch("/api/problems")
+    //fetchはレスポンスオブジェクトを返すのでJSON型に変換.
     .then((res) => res.json())
     .then((data) => setProblems(data));
   },[]);
-
-  async function handleAnswer(id: number,answer: boolean) {
+  //回答を送信し、正答判定を取得
+  async function handleAnswer(id: number,answer: number) {
     const res = await fetch("/api/problems",{
       method: "POST",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify({id,answer}),
     });
     const data = await res.json();
-    setMessage(data.message);
+    setMessages((prev) => ({
+      //全プロパティをコピー（既存の回答状況をコピー）
+      ...prev,
+      //新たに正答判定を追加
+      [id]:data.correct,
+    }))
     
   }
 
@@ -35,18 +44,19 @@ return(
 
     {problems.map((p) => (
       <div key={p.id} style={{marginBottom: 30}}>
+        <p>問題{p.id}</p>
         <p>{p.text}</p>
         {p.image && <img src={p.image} alt="問題画像" width={200}></img>}
         <div>
           <button 
-          onClick={() => handleAnswer(p.id,true)}
+          onClick={() => handleAnswer(p.id,1)}
            style={{
                 padding: "8px 16px",
                 fontSize: "1em",
                 cursor: "pointer",
               }}
               >◎</button>
-          <button onClick={() => handleAnswer(p.id,false)}
+          <button onClick={() => handleAnswer(p.id,0)}
              style={{
                 padding: "8px 16px",
                 fontSize: "1em",
@@ -54,9 +64,14 @@ return(
               }}
               >×</button>
         </div>
+        {/*回答判定されている場合のみ判定結果を出力 */}
+        {messages[p.id] !== undefined && (
+        <h3>{messages[p.id] ? "正解！" : "不正解"}</h3>
+        )}
   </div>
-))}
-  {message && <h3>{message}</h3>}
+)
+)}
+
   </div>
 );
 }
