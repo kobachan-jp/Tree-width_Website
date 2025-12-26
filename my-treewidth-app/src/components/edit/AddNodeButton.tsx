c3038c450a6bbd38dfdaf0b6354c53d18070ed39
kobachan <shoko.kobachan@gmail.com>
2025-12-26 12:08:01 +0900

    file構造の変更

---
+++ my-treewidth-app/src/components/edit/AddNodeButton.tsx
@@ -0,0 +1,9 @@
+import {Node} from 'reactflow';
+
+export default function CreatNode(label:string,index:number):Node {
+
+    return(
+        id : crypto.randomUUID(),
+        type: 
+    )
+}
---
+++ my-treewidth-app/src/components/edit/EditCustomNode.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { Handle, Position, NodeProps, EdgeLabelRenderer } from 'reactflow';
+import styles from './editCustomNode.module.css';
+
+export default function EditCustomNode({ data, selected }: NodeProps) {
+  return (
+    <div className={`${styles.node} ${selected ? styles.selected : ''}`}>
+      {data.label}
+
+
+      <Handle
+  type="source"
+  id="source"
+  position={Position.Bottom}
+  className={styles.sourceHandle}
+/>
+
+<Handle
+  type="target"
+  id="target"
+  position={Position.Top}
+  className={styles.targetHandle}
+/>
+
+    </div>
+  );
+}
